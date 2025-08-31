48f3c77664f9d371e82d036171a0149137f83a99
Matheus Cassemiro <matheus.simscampos@gmail.com>
2025-08-31 19:30:49 -0300

    refactor: visual alterado para assimilar uma caixa, paleta de cores em processo de troca

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,14 +2,17 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Login } from "../pages/login/login";
 import { Home } from "../pages/home/home";
+import "./routes.css"
 
 export const AppRoutes: React.FC = () => {
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Login/>}/>
-                <Route path="/home" element={<Home/>}/>
-            </Routes>
+            <div className='background-app'>
+                <Routes>
+                    <Route path="/" element={<Login />} />
+                    <Route path="/home" element={<Home />} />
+                </Routes>
+            </div>
         </BrowserRouter>
     );
 }
