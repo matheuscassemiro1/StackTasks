4cf667158f2c27d179af69dcb43d81b2f454436a
Matheus Cassemiro <matheus.simscampos@gmail.com>
2025-09-03 21:42:56 -0300

    refactor: visual da tela home alterado, iniciando novo método de apresentação da tela

--- src/components/taskFilter/taskFilter.tsx
+++ src/components/taskFilter/taskFilter.tsx
@@ -1,11 +1,28 @@
 import React from "react";
 import { Datalist, Input, Option } from "../../styles/global";
-
+import "./taskFilter.css"
 export const TaskFilter: React.FC = () => {
     return (
         <>
-            <div>
-                <span><i>Filtro</i></span>
+            <div className="divFilters">
+                <div className="topicFilters">
+                    <span>PROJETOS</span>
+                    <div>
+                        <div className="option">Cardboard</div>
+                    </div>
+                    <span>FILTROS</span>
+                    <div>
+                        <span className="option">Todas</span>
+                        <span className="option">Pendente</span>
+                    </div>
+                    
+                    <div>
+                    </div>
+                    <span>TAGS</span>
+                    <div>
+                        <div>tag</div>
+                    </div>
+                </div>
                 <hr />
                 <Input list="filtros" placeholder="Sem filtro" value="Ativas" $small></Input>
                 <Datalist id="filtros">
