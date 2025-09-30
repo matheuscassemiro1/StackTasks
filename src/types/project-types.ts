d3824592ae029b4ea57fc4896e96e1f56b01c724
Matheus Cassemiro <matheus.simscampos@gmail.com>
2025-09-29 22:06:01 -0300

    chore: altera nome da do arquivo de tipos de projeto e adiciona lista de projetos nele

--- src/types/project-types.ts
+++ src/types/project-types.ts
@@ -1,7 +1,14 @@
 import { Task } from "./task";
 
+
 export interface Project{
     id: number,
     nome: string,
     tasks?: Task[]  
+}
+
+export interface ProjectList{
+    id: number,
+    nome: string,
+    projects: Project[]
 }
